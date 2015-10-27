4b8b6aef4281d0fbf5fb07fea87b18c9570828bf
crTitan <colossusofRhodesTitan@gmail.com>
2015-10-27 13:25:37 -0400

    updates game graphics

--- core/src/com/seniorproject/game/BaseBullet.java
+++ core/src/com/seniorproject/game/BaseBullet.java
@@ -1,5 +1,12 @@
 package com.seniorproject.game;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class BaseBullet extends GameActor {
+
+	public BaseBullet(World world) {
+		super(world);
+		// TODO Auto-generated constructor stub
+	}
 
 }
